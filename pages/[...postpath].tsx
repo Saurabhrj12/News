6d8a94e39341cd1f1b06926a3123d28a3be077e3
Saurabhrj12 <153442631+Saurabhrj12@users.noreply.github.com>
2023-12-12 18:58:57 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,9 +82,9 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
+				{/* <meta property="og:title" content={post.title} /> */}
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
+				{/* <meta property="og:description" content={removeTags(post.excerpt)} /> */}
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
@@ -96,7 +96,7 @@ const Post: React.FC<PostProps> = (props) => {
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
 				/>
-				<title>{post.title}</title>
+				{/* <title>{post.title}</title> */}
 			</Head>
 			<div className="post-container">
 				<h1>{post.title}</h1>
